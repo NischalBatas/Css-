ed6254dd5a5b07077a2456c8602c130f362763ae
Batas <nischal.batas88@gmail.com>
2023-02-04 23:49:12 +0545

    'final'

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
 $(document).ready(function(){
-    $('.navbar_title_search').hide()
-   $('.navbar_menu_icon3').hide()
+    // $('.navbar_title_search').hide()
+    
+//    $('.navbar_menu_icon2').hide()
+//     $('.navbar_menu_icon2').click(()=>{
+//         $('.navbar_title_search').show()
+//         $('.navbar_menu_icon2').hide()
+//         $('.navbar_menu_icon3').show()
+     
+//     })
+
+//     $('.navbar_menu_icon3').click(()=>{
+//         $('.navbar_menu_icon2').show()
+//         $('.navbar_title_search').hide()
+//         $('.navbar_menu_icon3').hide()
+//     })
+
+//     $('.navbar_menu_icon').hide()
+//     $('.navbar_menu_icon4').click(()=>{
+//         $('.navbar_list').hide(1000)
+//         $('.navbar_menu_icon4').hide()
+//         $('.navbar_menu_icon').show()
+//     })
+
+//     $('.navbar_menu_icon').click(()=>{
+//         $('.navbar_list').show(1000)
+//         $('.navbar_menu_icon4').show()
+//         $('.navbar_menu_icon').hide()
+//     })
+
+    $('.navbar_menu_icon3').hide()
+    $('.navbar_menu_icon4').hide()
+
     $('.navbar_menu_icon2').click(()=>{
-        $('.navbar_title_search').show()
+        $('.navbar_title_search').addClass('searchfield')
         $('.navbar_menu_icon2').hide()
         $('.navbar_menu_icon3').show()
-     
     })
 
     $('.navbar_menu_icon3').click(()=>{
+        $('.navbar_title_search').removeClass('searchfield')
         $('.navbar_menu_icon2').show()
-        $('.navbar_title_search').hide()
         $('.navbar_menu_icon3').hide()
     })
+    
+    $('.navbar_menu_icon').click(()=>{
+        $('.navbar_list').addClass('itemslists')
+        $('.navbar_menu_icon4').show()
+        $('.navbar_menu_icon').hide()
 
+    })
+
+    $('.navbar_menu_icon4').click(()=>{
+        $('.navbar_list').removeClass('itemslists')
+        $('.navbar_menu_icon4').hide()
+        $('.navbar_menu_icon').show()
+
+    })
 
 });
+
+
